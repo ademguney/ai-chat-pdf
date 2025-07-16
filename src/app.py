357b6d185e8fa917112f45f470ba1d5affba7d5b
ademguney <adem.guney@outlook.com.tr>
2025-07-16 19:15:49 +0300

    feat(app.py): set up enviroment loading and basic Streamlit layout with OpenAI API key validation.

--- src/app.py
+++ src/app.py
@@ -1,1 +1,26 @@
-print("hello world")
+import os
+import streamlit as st
+from dotenv import load_dotenv
+
+from utils.pdf_utils import get_pdf_text
+from utils.qa_chain import get_conversational_chain
+from utils.embedding_utils import get_vectorstore, get_text_chunks
+
+
+
+# ---------- Step 1: Environment Setup ----------
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+if not openai_api_key:
+    st.error("You need to set an OpenAI API key")
+    st.stop()
+
+
+
+
+
+# ---------- Step 2: Streamlit Page Setup ----------
+st.set_page_config(page_title="Open Source Chat", page_icon=":books:", layout="wide")
+st.title("PDF Chatbot")
+st.write("Please upload your PDF files.")
